c21bb2522b82ed40019c41ba23a4e0c86e16fb76
ganishm <ganishronl@gmail.com>
2024-03-29 10:48:29 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return ()=> clearInterval(timer);
+  }, []);
+
+  const formatTimeWithLeadingZero = (num) => {
+    return num < 10 ? `0${num}` : num;
+  };
+
+  const formatHour = (hour) => {
+    return hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
+  };
+
+  const formetDate = (date) =>{
+    const options = {weekday: "long", year:"numeric", month: "long", day:"numeric"}
+    return date.toLocaleDateString(undefined, options)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="digital-clock">
+        <h1>Digital Clock</h1>
+        <div className="time">
+          {formatTimeWithLeadingZero(formatHour(currentTime.getHours()))} :
+          {formatTimeWithLeadingZero(currentTime.getMinutes())} :
+          {formatTimeWithLeadingZero(currentTime.getSeconds())}
+          {currentTime.getHours() >= 12? " PM": " AM"}
+        </div>
+        <div className="date">{formetDate(currentTime)}</div>
+      </div>
+    </>
   );
 }
 
